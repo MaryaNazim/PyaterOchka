fdd3e10baf5c64498e15a335a483a933ed9f5088
marya.nazimova03@mail.ru <marya.nazimova03@mail.ru>
2023-02-11 10:24:48 +0500

    абобаа

---
+++ DateTime/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+
+namespace DateTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Переменная соединения
+        MySqlConnection conn;
+        //DataAdapter представляет собой объект Command , получающий данные из источника данных.
+        private MySqlDataAdapter MyDA = new MySqlDataAdapter();
+        //Объявление BindingSource, основная его задача, это обеспечить унифицированный доступ к источнику данных.
+        private BindingSource bSource = new BindingSource();
+        //DataSet - расположенное в оперативной памяти представление данных, обеспечивающее согласованную реляционную программную 
+        //модель независимо от источника данных.DataSet представляет полный набор данных, включая таблицы, содержащие, упорядочивающие 
+        //и ограничивающие данные, а также связи между таблицами.
+        private DataSet ds = new DataSet();
+        //Представляет одну таблицу данных в памяти.
+        private DataTable table = new DataTable();
+        //Переменная для ID записи в БД, выбранной в гриде. Пока она не содердит значения, лучше его инициализировать с 0
+        //что бы в БД не отправлялся null
+        string id_selected_rows = "0";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // строка подключения к БД
+            string connStr = "server=10.90.12.110;port=33333;user=st_4_20_6;database=is_4_20_st6_KURS;password=22702128;";
+            // создаём объект для подключения к БД
+            conn = new MySqlConnection(connStr);
+            //Вызываем метод для заполнение дата Грида
+            GetListProducts();
+            //Видимость полей в гриде
+            dataGridView1.Columns[0].Visible = true;
+            dataGridView1.Columns[1].Visible = true;
+            dataGridView1.Columns[2].Visible = true;
+            dataGridView1.Columns[3].Visible = true;
+
+            //Ширина полей
+            dataGridView1.Columns[0].FillWeight = 15;
+            dataGridView1.Columns[1].FillWeight = 40;
+            dataGridView1.Columns[2].FillWeight = 15;
+            dataGridView1.Columns[3].FillWeight = 15;
+            //Режим для полей "Только для чтения"
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[2].ReadOnly = true;
+            dataGridView1.Columns[3].ReadOnly = true;
+            //Растягивание полей грида
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            //Убираем заголовки строк
+            dataGridView1.RowHeadersVisible = false;
+            //Показываем заголовки столбцов
+            dataGridView1.ColumnHeadersVisible = true;
+            //Вызываем метод покраски ДатаГрид
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Right))
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+                //dataGridView1.CurrentRow.Selected = true;
+                dataGridView1.CurrentCell.Selected = true;
+                GetSelectedIDString();
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //Магические строки
+            dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+            dataGridView1.CurrentRow.Selected = true;
+            GetSelectedIDString();
+        }
+
+        //Метод обновления DataGreed
+        public void reload_list()
+        {
+            //Чистим виртуальную таблицу
+            table.Clear();
+            //Вызываем метод получения записей, который вновь заполнит таблицу
+            GetListProducts();
+
+        }
+
+        //Метод получения ID выделенной строки, для последующего вызова его в нужных методах
+        public void GetSelectedIDString()
+        {
+            //Переменная для индекс выбранной строки в гриде
+            string index_selected_rows;
+            //Индекс выбранной строки
+            index_selected_rows = dataGridView1.SelectedCells[0].RowIndex.ToString();
+            //ID конкретной записи в Базе данных, на основании индекса строки
+            id_selected_rows = dataGridView1.Rows[Convert.ToInt32(index_selected_rows)].Cells[0].Value.ToString();
+
+        }
+
+        //Метод наполнения виртуальной таблицы и присвоение её к датагриду
+        public void GetListProducts()
+        {
+            //Запрос для вывода строк в БД
+            string commandStr = "SELECT id, title AS 'Название продукта', price AS 'Цена', date_expiration AS 'Срок годности' FROM pyaterochka";
+            //Открываем соединение
+            conn.Open();
+            //Объявляем команду, которая выполнить запрос в соединении conn
+            MyDA.SelectCommand = new MySqlCommand(commandStr, conn);
+            //Заполняем таблицу записями из БД
+            MyDA.Fill(table);
+            //Указываем, что источником данных в bindingsource является заполненная выше таблица
+            bSource.DataSource = table;
+            //Указываем, что источником данных ДатаГрида является bindingsource 
+            dataGridView1.DataSource = bSource;
+            dataGridView1.Invoke(new Action(() =>
+            {
+                System.DateTime editDt = System.DateTime.Now.AddDays(1);
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    System.DateTime date = Convert.ToDateTime(row.Cells[3].Value);
+                    if (date < System.DateTime.Now.AddDays(-1))
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Red;
+                    }
+                    else if (date > System.DateTime.Now.AddDays(-1) && date < editDt)
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Yellow;
+                    }
+                }
+            }));
+            conn.Close();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tmp = $"INSERT INTO pyaterochka (title, price, date_expiration) VALUES ('{textBox1.Text}', '{textBox2.Text}', '{dateTimePicker1.Value.ToString("yyyy-MM-dd")}')";
+            MySqlCommand cmd = new MySqlCommand(tmp, conn);
+            try
+            {
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Добавление прошло успешно");
+                reload_list();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Формируем строку запроса на добавление строк
+            string sql_delete = "DELETE FROM pyaterochka WHERE id='" + id_selected_rows + "'";
+            //Посылаем запрос на обновление данных
+            MySqlCommand delete = new MySqlCommand(sql_delete, conn);
+            try
+            {
+                conn.Open();
+                delete.ExecuteNonQuery();
+                MessageBox.Show("Удаление прошло успешно", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка удаления строки \n" + ex, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            finally
+            {
+                conn.Close();
+                reload_list();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            bSource.Filter = "[title] LIKE'" + textBox3.Text + "%'";
+        }
+    }
+}
